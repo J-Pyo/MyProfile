3b07307da93bbc2bd53b6ac66cee3de70eabef91
홍정표 <hongjeongpyo@hongjeongpyoui-MacBookPro.local>
2020-03-18 13:46:50 +0900

    my profile auto layout

---
+++ MyProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProfile
+//
+//  Created by 홍정표 on 2020/03/06.
+//  Copyright © 2020 홍정표. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
